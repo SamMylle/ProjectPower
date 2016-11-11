43fd6aa6bc7015481b21810ce032ae16257da98c
Cpt_McMylle <mylle.sam@gmail.com>
2016-11-11 20:33:14 +0100

    Controller ID and clientType, tests written for that stuff as well

--- playground/sam/Early Work prototype/Controller.java
+++ playground/sam/Early Work prototype/Controller.java
@@ -2,7 +2,7 @@ package controller;
 
 import java.util.Queue;
 import java.util.Vector;
-import java.util.Dictionary;
+import java.util.HashMap;
 
 import util.ClientType;
 
@@ -10,13 +10,28 @@ public class Controller {
 	public Controller(int maxTemperatures){
 		f_nextID = 0;
 		f_maxTemperatures = maxTemperatures;
+		f_names = new HashMap<Integer, ClientType>();
 	}
 	
 	public int giveNextID(ClientType type){
+		f_names.put(f_nextID, type);
 		int ID = f_nextID;
 		f_nextID++;
-		f_names.put(ID, type);
 		return ID;
+	}
+	
+	public ClientType getClientType(int ID){
+		try{
+			return f_names.get(ID);
+		}catch(NullPointerException e){
+			return null;
+		}
+	}
+	
+	public void removeID(int ID){
+		try{
+			f_names.remove(ID);
+		}catch(NullPointerException e){}
 	}
 	
 	public boolean unusedID(int ID){
@@ -42,8 +57,10 @@ public class Controller {
 		return average;
 	}
 	
+	
+	
 	private int f_nextID;
 	private final int f_maxTemperatures;
-	private Dictionary<Integer, ClientType> f_names;
+	private HashMap<Integer, ClientType> f_names;
 	private Queue<Double> f_temperatures;
 }
