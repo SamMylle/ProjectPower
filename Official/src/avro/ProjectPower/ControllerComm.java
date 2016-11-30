5f98405344f0e6e55d35c165bc095e19e96e2a3d
Cpt_McMylle <mylle.sam@gmail.com>
2016-11-30 23:56:13 +0100

    Get Clients from controller

--- Official/src/avro/ProjectPower/ControllerComm.java
+++ Official/src/avro/ProjectPower/ControllerComm.java
@@ -8,7 +8,7 @@ package avro.ProjectPower;
 @SuppressWarnings("all")
 @org.apache.avro.specific.AvroGenerated
 public interface ControllerComm {
-  public static final org.apache.avro.Protocol PROTOCOL = org.apache.avro.Protocol.parse("{\"protocol\":\"ControllerComm\",\"namespace\":\"avro.ProjectPower\",\"types\":[{\"type\":\"enum\",\"name\":\"ClientType\",\"symbols\":[\"Light\",\"SmartFridge\",\"User\",\"TemperatureSensor\"]}],\"messages\":{\"getID\":{\"request\":[{\"name\":\"type\",\"type\":\"ClientType\"}],\"response\":\"int\"},\"getClientType\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"ClientType\"},\"logOff\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"null\"},\"addTemperature\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"},{\"name\":\"temperature\",\"type\":\"double\"}],\"response\":\"null\"},\"averageCurrentTemperature\":{\"request\":[],\"response\":\"double\"},\"hasValidTemperatures\":{\"request\":[],\"response\":\"boolean\"},\"setupFridgeCommunication\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"int\"},\"listenToMe\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"},{\"name\":\"type\",\"type\":\"ClientType\"}],\"response\":\"null\"},\"getFridgeInventory\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":{\"type\":\"array\",\"items\":\"string\"}},\"setLight\":{\"request\":[{\"name\":\"newState\",\"type\":\"int\"},{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"int\"},\"getLightState\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"int\"}}}");
+  public static final org.apache.avro.Protocol PROTOCOL = org.apache.avro.Protocol.parse("{\"protocol\":\"ControllerComm\",\"namespace\":\"avro.ProjectPower\",\"types\":[{\"type\":\"enum\",\"name\":\"ClientType\",\"symbols\":[\"Light\",\"SmartFridge\",\"User\",\"TemperatureSensor\"]},{\"type\":\"record\",\"name\":\"Client\",\"fields\":[{\"name\":\"clientType\",\"type\":\"ClientType\"},{\"name\":\"ID\",\"type\":\"int\"}]}],\"messages\":{\"getID\":{\"request\":[{\"name\":\"type\",\"type\":\"ClientType\"}],\"response\":\"int\"},\"getClientType\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"ClientType\"},\"logOff\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"null\"},\"addTemperature\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"},{\"name\":\"temperature\",\"type\":\"double\"}],\"response\":\"null\"},\"averageCurrentTemperature\":{\"request\":[],\"response\":\"double\"},\"hasValidTemperatures\":{\"request\":[],\"response\":\"boolean\"},\"setupFridgeCommunication\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"int\"},\"listenToMe\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"},{\"name\":\"type\",\"type\":\"ClientType\"}],\"response\":\"null\"},\"getFridgeInventory\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":{\"type\":\"array\",\"items\":\"string\"}},\"setLight\":{\"request\":[{\"name\":\"newState\",\"type\":\"int\"},{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"int\"},\"getLightState\":{\"request\":[{\"name\":\"ID\",\"type\":\"int\"}],\"response\":\"int\"},\"getAllClients\":{\"request\":[],\"response\":{\"type\":\"array\",\"items\":\"Client\"}}}}");
   int getID(avro.ProjectPower.ClientType type) throws org.apache.avro.AvroRemoteException;
   avro.ProjectPower.ClientType getClientType(int ID) throws org.apache.avro.AvroRemoteException;
   java.lang.Void logOff(int ID) throws org.apache.avro.AvroRemoteException;
@@ -20,6 +20,7 @@ public interface ControllerComm {
   java.util.List<java.lang.CharSequence> getFridgeInventory(int ID) throws org.apache.avro.AvroRemoteException;
   int setLight(int newState, int ID) throws org.apache.avro.AvroRemoteException;
   int getLightState(int ID) throws org.apache.avro.AvroRemoteException;
+  java.util.List<avro.ProjectPower.Client> getAllClients() throws org.apache.avro.AvroRemoteException;
 
   @SuppressWarnings("all")
   public interface Callback extends ControllerComm {
@@ -35,5 +36,6 @@ public interface ControllerComm {
     void getFridgeInventory(int ID, org.apache.avro.ipc.Callback<java.util.List<java.lang.CharSequence>> callback) throws java.io.IOException;
     void setLight(int newState, int ID, org.apache.avro.ipc.Callback<java.lang.Integer> callback) throws java.io.IOException;
     void getLightState(int ID, org.apache.avro.ipc.Callback<java.lang.Integer> callback) throws java.io.IOException;
+    void getAllClients(org.apache.avro.ipc.Callback<java.util.List<avro.ProjectPower.Client>> callback) throws java.io.IOException;
   }
 }
--- Official/src/controller/DistController.java
+++ Official/src/controller/DistController.java
@@ -17,297 +17,312 @@ import client.SmartFridge;
 import util.Logger;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.Vector;
 import java.util.concurrent.ExecutionException;
 
 import avro.ProjectPower.*;
 
 public class DistController extends Controller implements ControllerComm, Runnable{
-	
-		//private Controller f_controller;
-		private Server f_server;
-		private Thread f_serverThread;
-		private int f_myPort;
-		private boolean f_serverActive;
-		
-		public DistController(int port, int maxTemperatures){
-			super(port + 1, maxTemperatures);
-			//f_controller = new Controller(port + 1, 10);
-			f_myPort = port;
-			f_serverActive = false;
-			
-			/// Make a thread, the "run" method will execute in a new thread
-			/// The run method must be implemented by a Runnable object (see implements in this class)
-			/// Since the server must run on this object, "this" is passed to the thread
-			/// Below, when starting the thread (thread.start()), the thread will call this.run()
-			/// This has to be this way because the server doesn't get out of the eternal loop
-			/// This object wouldn't be able to do other interesting stuff if it wasn't for the threads
-			f_serverThread = new Thread(this);
-			f_serverThread.start();
-			
-			while (!f_serverActive){
-				try {
-					Thread.sleep(50);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					Logger.getLogger().log("Server startup failed");
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		public boolean serverIsActive(){
-			return f_serverActive;
-		}
-		
-		@Override
-		public void run() {
-			/// when thread.start() is invoked, this method is ran
-			try{
-				f_server = new SaslSocketServer(
-						new SpecificResponder(ControllerComm.class,
-						this), new InetSocketAddress(f_myPort));
-			}catch(IOException e){
-				System.err.println("[error]Failed to start server");
-				e.printStackTrace(System.err);
-				System.exit(1);
-			}
-			f_server.start();
-			f_serverActive = true;
-			try{
-				f_server.join();
-			}catch(InterruptedException e){
-				f_server.close();
-			}
-		}
-		
-		public void stopServer(){
-			f_serverThread.interrupt();
-			f_serverActive = false;
-		}
-		
-		public Transceiver setupTransceiver(int ID){
-			try{
-				Transceiver client = new SaslSocketTransceiver(new InetSocketAddress(ID));
-				return client;
-			}catch(IOException e){
-				System.err.println("Error connecting to the client server...");
-				return null;
-			}
-		}
-		
-		@Override
-		public int getID(ClientType clientType) throws AvroRemoteException{
-			Logger.getLogger().log("give new ID");
-			int newID = this.giveNextID(clientType);
-			return newID;
-		}
-		
-		@Override
-		public ClientType getClientType(int ID) throws AvroRemoteException{
-			return this.getClType(ID);
-		}
-		
-		@Override
-		public Void logOff(int ID) throws AvroRemoteException{
-			/// Remove ID from the system
-			/// TODO, special case when the client is a temperatureSensor
-			this.removeID(ID);
-			
-			return null;
-		}
-		
-		@Override
-		public java.lang.Void addTemperature(int ID, double temperature) throws AvroRemoteException{
-			this.addTemperature(temperature, ID);
-			return null;
-		}
-		
-		@Override
-		public double averageCurrentTemperature() throws AvroRemoteException{
-			return this.averageCurrentTemp();
-		}
-		
-		@Override
-		public boolean hasValidTemperatures() throws AvroRemoteException{
-			return this.hasValidTemp();
-		}
-		
-		@Override
-		public int setupFridgeCommunication(int ID) throws AvroRemoteException {
-			try {
-				Transceiver client = this.setupTransceiver(ID);
-				
-				if (client == null){
-					return -1;
-				}
-				
-				/// Connect to fridge
-				communicationFridge.Callback proxy =
-						SpecificRequestor.getClient(communicationFridge.Callback.class, client);
-				
-				/// Ask the fridge if it's okay to connect a user to it
-				if (proxy.requestFridgeCommunication() == true){
-					return ID;
-				}else{
-					return -1;
-				}
-			}catch(IOException e){
-				return -1;
-			}
-		}
 
-		@Override
-		@Deprecated
-		public Void listenToMe(int port, ClientType type) throws AvroRemoteException {
-			/// Remote call by e.g. a fridge, to indicate the server can reach him on this port (usually the ID of the client)
-			// TODO check if the ID is in the system and in the transceivers
-			// TODO maybe change retval to boolean
-			//this.setupTransceiver(type, port);
-			return null;
-		}
+	//private Controller f_controller;
+	private Server f_server;
+	private Thread f_serverThread;
+	private int f_myPort;
+	private boolean f_serverActive;
 
-		@Override
-		public List<CharSequence> getFridgeInventory(int ID) throws AvroRemoteException {
-			/// return null on invalid stuff and thangs
-			ClientType type = this.f_names.get(ID);
-			
-			if(type != ClientType.SmartFridge){
-				return null;
-			}
-			
-			// We know the type is a fridge (AND it exists)
-			Transceiver fridge = this.setupTransceiver(ID);
-			
-			if (fridge == null){
-				// If connection can't be established, just say no to the other guy
-				return null;
-			}
-			
-			/// get the inventory and return it
-			/// TODO stuff
-			communicationFridge.Callback proxy;
+	public DistController(int port, int maxTemperatures){
+		super(port + 1, maxTemperatures);
+		//f_controller = new Controller(port + 1, 10);
+		f_myPort = port;
+		f_serverActive = false;
+
+		/// Make a thread, the "run" method will execute in a new thread
+		/// The run method must be implemented by a Runnable object (see implements in this class)
+		/// Since the server must run on this object, "this" is passed to the thread
+		/// Below, when starting the thread (thread.start()), the thread will call this.run()
+		/// This has to be this way because the server doesn't get out of the eternal loop
+		/// This object wouldn't be able to do other interesting stuff if it wasn't for the threads
+		f_serverThread = new Thread(this);
+		f_serverThread.start();
+
+		while (!f_serverActive){
 			try {
-				proxy = SpecificRequestor.getClient(communicationFridge.Callback.class, fridge);
-			} catch (IOException e) {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
+				Logger.getLogger().log("Server startup failed");
 				e.printStackTrace();
-				return null;
-			}
-			
-			return proxy.getItemsRemote();
-		}
-
-		@Override
-		public int setLight(int newState, int ID) throws AvroRemoteException {
-			/// return -1 on invalid stuff and thangs
-			/// return 0 on success
-			
-			ClientType type = this.f_names.get(ID);
-			
-			if(type != ClientType.Light){
-				return -1;
-			}
-			
-			// We know the type is a light (AND it exists)
-			Transceiver light = this.setupTransceiver(ID);
-			
-			if (light == null){
-				// If connection can't be established, just say no to the other guy
-				return -1;
-			}
-
-			try {
-				/// set the state and thangs
-				LightComm.Callback proxy;
-					proxy = SpecificRequestor.getClient(LightComm.Callback.class, light);
-
-					proxy.setState(newState);
-
-					return 0;
-			} catch (IOException e) {
-				// TODO remove from system?
-				e.printStackTrace();
-				Logger.getLogger().log("IOEXCEPT for remote light setstate");
-				return -1;
 			}
 		}
+	}
 
-		@Override
-		public int getLightState(int ID) throws AvroRemoteException {
-			ClientType type = this.f_names.get(ID);
-			
-			if(type != ClientType.Light){
+	public boolean serverIsActive(){
+		return f_serverActive;
+	}
+
+	@Override
+	public void run() {
+		/// when thread.start() is invoked, this method is ran
+		try{
+			f_server = new SaslSocketServer(
+					new SpecificResponder(ControllerComm.class,
+							this), new InetSocketAddress(f_myPort));
+		}catch(IOException e){
+			System.err.println("[error]Failed to start server");
+			e.printStackTrace(System.err);
+			System.exit(1);
+		}
+		f_server.start();
+		f_serverActive = true;
+		try{
+			f_server.join();
+		}catch(InterruptedException e){
+			f_server.close();
+		}
+	}
+
+	public void stopServer(){
+		f_serverThread.interrupt();
+		f_serverActive = false;
+	}
+
+	private Transceiver setupTransceiver(int ID){
+		try{
+			Transceiver client = new SaslSocketTransceiver(new InetSocketAddress(ID));
+			return client;
+		}catch(IOException e){
+			System.err.println("Error connecting to the client server...");
+			return null;
+		}
+	}
+
+	@Override
+	public int getID(ClientType clientType) throws AvroRemoteException{
+		/// TODO rename to login
+		Logger.getLogger().log("give new ID");
+		int newID = this.giveNextID(clientType);
+		return newID;
+	}
+
+	@Override
+	public ClientType getClientType(int ID) throws AvroRemoteException{
+		return this.getClType(ID);
+	}
+
+	@Override
+	public Void logOff(int ID) throws AvroRemoteException{
+		/// Remove ID from the system
+		/// TODO, special case when the client is a temperatureSensor
+		this.removeID(ID);
+
+		return null;
+	}
+
+	@Override
+	public java.lang.Void addTemperature(int ID, double temperature) throws AvroRemoteException{
+		this.addTemperature(temperature, ID);
+		return null;
+	}
+
+	@Override
+	public double averageCurrentTemperature() throws AvroRemoteException{
+		return this.averageCurrentTemp();
+	}
+
+	@Override
+	public boolean hasValidTemperatures() throws AvroRemoteException{
+		return this.hasValidTemp();
+	}
+
+	@Override
+	public int setupFridgeCommunication(int ID) throws AvroRemoteException {
+		try {
+			Transceiver client = this.setupTransceiver(ID);
+
+			if (client == null){
 				return -1;
 			}
-			
-			// We know the type is a light (AND it exists)
-			SaslSocketTransceiver light = null;
-			try {
-				light = new SaslSocketTransceiver(new InetSocketAddress(ID));
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				
-				Logger.getLogger().log("EEK");
-				e1.printStackTrace();
-				System.exit(0);
+
+			/// Connect to fridge
+			communicationFridge.Callback proxy =
+					SpecificRequestor.getClient(communicationFridge.Callback.class, client);
+
+			/// Ask the fridge if it's okay to connect a user to it
+			if (proxy.requestFridgeCommunication() == true){
+				return ID;
+			}else{
+				return -1;
 			}
-			
-			/*if (light == null){
+		}catch(IOException e){
+			return -1;
+		}
+	}
+
+	@Override
+	@Deprecated
+	public Void listenToMe(int port, ClientType type) throws AvroRemoteException {
+		/// Remote call by e.g. a fridge, to indicate the server can reach him on this port (usually the ID of the client)
+		// TODO check if the ID is in the system and in the transceivers
+		// TODO maybe change retval to boolean
+		//this.setupTransceiver(type, port);
+		return null;
+	}
+
+	@Override
+	public List<CharSequence> getFridgeInventory(int ID) throws AvroRemoteException {
+		/// return null on invalid stuff and thangs
+		ClientType type = this.f_names.get(ID);
+
+		if(type != ClientType.SmartFridge){
+			return null;
+		}
+
+		// We know the type is a fridge (AND it exists)
+		Transceiver fridge = this.setupTransceiver(ID);
+
+		if (fridge == null){
+			// If connection can't be established, just say no to the other guy
+			return null;
+		}
+
+		/// get the inventory and return it
+		/// TODO stuff
+		communicationFridge.Callback proxy;
+		try {
+			proxy = SpecificRequestor.getClient(communicationFridge.Callback.class, fridge);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+		return proxy.getItemsRemote();
+	}
+
+	@Override
+	public int setLight(int newState, int ID) throws AvroRemoteException {
+		/// return -1 on invalid stuff and thangs
+		/// return 0 on success
+
+		ClientType type = this.f_names.get(ID);
+
+		if(type != ClientType.Light){
+			return -1;
+		}
+
+		// We know the type is a light (AND it exists)
+		Transceiver light = this.setupTransceiver(ID);
+
+		if (light == null){
+			// If connection can't be established, just say no to the other guy
+			return -1;
+		}
+
+		try {
+			/// set the state and thangs
+			LightComm.Callback proxy;
+			proxy = SpecificRequestor.getClient(LightComm.Callback.class, light);
+
+			proxy.setState(newState);
+
+			return 0;
+		} catch (IOException e) {
+			// TODO remove from system?
+			e.printStackTrace();
+			Logger.getLogger().log("IOEXCEPT for remote light setstate");
+			return -1;
+		}
+	}
+
+	@Override
+	public int getLightState(int ID) throws AvroRemoteException {
+		ClientType type = this.f_names.get(ID);
+
+		if(type != ClientType.Light){
+			return -1;
+		}
+
+		// We know the type is a light (AND it exists)
+		SaslSocketTransceiver light = null;
+		try {
+			light = new SaslSocketTransceiver(new InetSocketAddress(ID));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			System.exit(0);
+		}
+
+		/*if (light == null){
 				// This shouldn't happen actually, but you never know
 				this.setupTransceiver(ClientType.Light, ID);
 				light = f_transceivers.get(ID);
 			}*/
-			
-			if (light == null){
-				// If connection can't be established, just say no to the other guy
-				return -1;
-			}
 
-			try {
-				/// set the state and thangs
-				LightComm.Callback proxy;
-					proxy = SpecificRequestor.getClient(LightComm.Callback.class, light);
-
-					
-
-					return proxy.getState();
-			} catch (IOException e) {
-				// TODO remove from system?
-				e.printStackTrace();
-				Logger.getLogger().log("IOEXCEPT for remote light setstate");
-				return -1;
-			}
-
+		if (light == null){
+			// If connection can't be established, just say no to the other guy
+			return -1;
 		}
-		
-		public static void main(String[] args) {
-			DistController controller = new DistController(5000, 10);
-			
-			DistSmartFridge fridge = new DistSmartFridge(5000);
-			try {
-				fridge.addItemRemote("bacon");
-				fridge.addItemRemote("parmesan cheese");
-			} catch (AvroRemoteException e1) {
-				// TODO Auto-generated catch block
-				Logger.getLogger().log("woops");
-			}
-			
-			Logger.getLogger().log("Servers started");
-			
-			try {
-				List<CharSequence> items = controller.getFridgeInventory(5001);
-				
-				for (int i = 0; i < items.size(); i++){
-					Logger.getLogger().log(items.get(i).toString());
-				}
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+		try {
+			/// set the state and thangs
+			LightComm.Callback proxy;
+			proxy = SpecificRequestor.getClient(LightComm.Callback.class, light);
+
+
+
+			return proxy.getState();
+		} catch (IOException e) {
+			// TODO remove from system?
+			e.printStackTrace();
+			Logger.getLogger().log("IOEXCEPT for remote light setstate");
+			return -1;
 		}
-		
+
+	}
+
+	@Override
+	public java.util.List<Client> getAllClients() throws AvroRemoteException {
+		List<Client> ret = new Vector<Client>();
+
+		/// Ugliest for loop in the history of for loops
+		for (Map.Entry<Integer, ClientType> entry : f_names.entrySet()){
+			Client newClient = new Client(entry.getValue(), entry.getKey());
+			ret.add(newClient);
+		}
+
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		DistController controller = new DistController(5000, 10);
+
+		DistSmartFridge fridge = new DistSmartFridge(5000);
+		try {
+			fridge.addItemRemote("bacon");
+			fridge.addItemRemote("parmesan cheese");
+		} catch (AvroRemoteException e1) {
+			// TODO Auto-generated catch block
+			Logger.getLogger().log("woops");
+		}
+
+		Logger.getLogger().log("Servers started");
+
+		try {
+			List<CharSequence> items = controller.getFridgeInventory(5001);
+
+			for (int i = 0; i < items.size(); i++){
+				Logger.getLogger().log(items.get(i).toString());
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 
 }
