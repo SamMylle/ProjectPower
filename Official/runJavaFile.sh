dcb4fc407ac0f702bf68ba63752cf9e0f0860d3c
FedericoQuin <federico-quin@hotmail.com>
2016-12-07 01:22:43 +0100

    Wrote out an sh file which compiles and runs java files.

--- Official/runJavaFile.sh
+++ Official/runJavaFile.sh
@@ -4,8 +4,14 @@ if [ "$#" -ne 1 ]; then
 	echo "Enter an argument with a file (including its package(s)) that you want to run."
 	echo "For example: client.DistUser"
 else
-	java -cp src:lib/avro-1.7.7.jar:lib/avro-ipc-1.7.7.jar:lib/hamcrest-core-1.3.jar:lib/jackson-core-asl-1.9.13.jar:lib/jackson-mapper-asl-1.9.13.jar:lib/junit-4.12.jar:lib/slf4j-api-1.7.7.jar:lib/slf4j-simple-1.7.7.jar "$1"
-	# java -cp src:lib/avro-1.7.7.jar:lib/avro-ipc-1.7.7.jar:./lib/hamcrest-core-1.3.jar:./lib/jackson-core-asl-1.9.13.jar:./lib/jackson-mapper-asl-1.9.13.jar:./lib/junit-4.12.jar:./lib/slf4j-api-1.7.7.jar:./lib/slf4j-simple-1.7.7.jar "$1"
+	stringClass="$1"
+	stringPath=$(echo $stringClass | sed 's/\./\//g')
+	echo "Compiling the file..."
+	javac -d bin -sourcepath src -cp bin:lib/avro-1.7.7.jar:lib/avro-ipc-1.7.7.jar:lib/hamcrest-core-1.3.jar:lib/jackson-core-asl-1.9.13.jar:lib/jackson-mapper-asl-1.9.13.jar:lib/junit-4.12.jar:lib/slf4j-api-1.7.7.jar:lib/slf4j-simple-1.7.7.jar "src/$stringPath.java"> /dev/null 2>&1
+	
+	echo "Running the file..."
+	echo ""
+	java -cp bin:lib/avro-1.7.7.jar:lib/avro-ipc-1.7.7.jar:lib/hamcrest-core-1.3.jar:lib/jackson-core-asl-1.9.13.jar:lib/jackson-mapper-asl-1.9.13.jar:lib/junit-4.12.jar:lib/slf4j-api-1.7.7.jar:lib/slf4j-simple-1.7.7.jar "$1"
 fi
 
 
